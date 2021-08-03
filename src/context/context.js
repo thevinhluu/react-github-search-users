0d18ee49c0bc6bc91d16c6d51d9b2c731acca473
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-03 21:56:51 +0700

    repos and followers

--- src/context/context.js
+++ src/context/context.js
@@ -28,7 +28,16 @@ const GithubProvider = ({ children }) => {
 
 		if (response) {
 			setGithubUser(response.data);
+			const { login, followers_url } = response.data;
+			// repos
+			axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => setRepos(response.data));
+			// followes
+			axios(`${followers_url}?per_page=100`).then((response) => setFollowers(response.data));
 			// more logic here
+			// repos
+			// https://api.github.com/users/john-smilga/repos?per_page=100
+			// followers
+			// https://api.github.com/users/john-smilga/followers
 		}
 		else {
 			toggleError(true, 'there is no user with that username');
