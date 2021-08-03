2351fdff7fab3b1237983c7bbca37ebfa9776571
Luu The Vinh <luuthevinh1988@gmail.com>
2021-08-03 16:06:47 +0700

    stars and forks - functionality

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -40,30 +40,28 @@ const Repos = () => {
 		})
 		.slice(0, 5);
 
-	console.log(mostPopular);
+	// stars, forks
 
-	const chartData = [
-		{
-			label : 'HTML',
-			value : 30
+	let { stars, forks } = repos.reduce(
+		(total, item) => {
+			const { stargazers_count, name, forks } = item;
+			total.stars[stargazers_count] = { label: name, value: stargazers_count };
+			total.forks[forks] = { label: name, value: forks };
+			return total;
 		},
-		{
-			label : 'CSS',
-			value : 60
-		},
-		{
-			label : 'JS',
-			value : 10
-		}
-	];
+		{ stars: {}, forks: {} }
+	);
+
+	stars = Object.values(stars).slice(-5).reverse();
+	forks = Object.values(forks).slice(-5).reverse();
 
 	return (
 		<section className='section'>
 			<Wrapper className='section-center'>
 				<Pie3D data={mostUsed} />
-				<Column3D data={chartData} />
+				<Column3D data={stars} />
 				<Doughnut2D data={mostPopular} />
-				<Bar3D data={chartData} />
+				<Bar3D data={forks} />
 			</Wrapper>
 		</section>
 	);
